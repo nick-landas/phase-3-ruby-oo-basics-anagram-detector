b4d08fe2485caaa0f7633220f45b04bd1584065f
Nick Landas <nick.landas@gmail.com>
2022-12-18 18:25:07 -0600

    commit tree a7c2320661623beb4e09792f7c09938e9aaa444d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matching_word = []
+        array.each do |word| 
+            if word.chars.sort == @word.chars.sort 
+                matching_word << word 
+            end
+        end
+            matching_word
+    end
+    
+
+end
